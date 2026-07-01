903b9ab56c886bc00bd594e805a3e46371cbd85d
NStec-26004 <nstec26_004@fullness.email>
2026-07-01 09:23:52 +0900

    ソースコード修正

--- app/src/main/java/numer0n/App.java
+++ app/src/main/java/numer0n/App.java
@@ -9,7 +9,8 @@ public class App {
 
     public static void main(String[] args) {
         // ゲームを開始
-        GameManagerImpl gamemane = new GameManagerImpl();
-        gamemane.start();
+        GameManagerImpl gameman = new GameManagerImpl();
+        gameman.start();
+        //修正
     }
 }
